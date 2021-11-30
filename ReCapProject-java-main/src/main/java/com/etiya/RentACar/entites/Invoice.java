03c407d5dc77f55a53a2c2854ba1ca67f1b214c4
erdituna <erdituna36@gmail.com>
2021-11-30 17:06:58 +0300

    Requirement 14 started

---
+++ ReCapProject-java-main/src/main/java/com/etiya/RentACar/entites/Invoice.java
@@ -0,0 +1,52 @@
+package com.etiya.RentACar.entites;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="invoices")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class Invoice {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="invoice_id")
+	private int invoiceId;
+	
+	@Column(name="invoice_number")
+	private int invoiceNumber;
+	
+	@Column(name="invoice_date")
+	private LocalDate invoiceDate;
+	
+	@Column(name="total_rental_day")
+	private int totalRentalDay;
+	
+	@Column(name="total_amount")
+	private double totalAmount;
+
+	@OneToOne
+	@JoinColumn(name="rental_id")
+	private Rental rental;
+	
+	
+}
